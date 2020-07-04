38f0bc2a6b6be73d42697c3f7c5190521a177793
Damián Parrino <bucanero@users.noreply.github.com>
2020-07-04 17:40:39 -0300

    Create utime.h
    Add utime() definition.

---
+++ newlib/libc/sys/lv2/sys/utime.h
@@ -0,0 +1,20 @@
+#ifndef _SYS_UTIME_H
+#define _SYS_UTIME_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct utimbuf 
+{
+  time_t actime;
+  time_t modtime; 
+};
+
+int utime(const char *path, const struct utimbuf *times);
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif /* _SYS_UTIME_H */
